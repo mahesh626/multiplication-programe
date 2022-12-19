8906638018c594ff00f3540677bc790a8c6c7105
mahesh626 <121013176+mahesh626@users.noreply.github.com>
2022-12-19 14:20:17 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Table
+{
+static public void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter n vlaue");
+int n=s.nextInt();
+System.out.println("enter table");
+int t=s.nextInt();
+for(int i=1;i<=n;i++)
+{
+System.out.println("t*i="i*t);
+}
+}
+}
